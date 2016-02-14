9ee12b54725ddb064ad789404a0912e487846b73
Linhai Yin <yinlinhai@gmail.com>
2016-02-13 20:08:15 -0500

    Fixed for python 3
    Python 3 is stupid and should burn in hell.

--- ImageScanner.py
+++ ImageScanner.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request
 import subprocess
 
 EAN_ACCESS_CODE = "F2EF335156BEAF07"
@@ -7,9 +7,9 @@ def get_upc(fileName):
     '''(str) -> str
     gets the upc from an image.
     '''
-    output = subprocess.check_output("zbarimg -D " + fileName)
+    output = subprocess.getoutput(["zbarimg", "-D", fileName])
     output = output.strip()
-    output = output[output.index(':')+1:]
+    output = output[output.index(':')+1:output.index('\n')]
     return output
     
 def get_data(upc_code):
@@ -17,20 +17,22 @@ def get_data(upc_code):
     accesses EANdata.com with free data stream for data on product
     '''
 
-    response = urllib2.urlopen("http://eandata.com/feed/?v=3&mode=json&keycode="
+    response = urllib.request.Request("http://eandata.com/feed/?v=3&mode=json&keycode="
                                + EAN_ACCESS_CODE + "&find=" + upc_code)
-    data = response.read()
+    response = urllib.request.urlopen(response)
+    response = response.read()
+    data = str(response)
+    
+    print(data)
     if data.find('company') != -1:
         
         company = data[data.find('company')-1:len(data)-2]
     
         company_name = company[company.find('name')+7:company.find('logo')-3]
-        company_name = company[:company.find(' ')]
-        print company_name
+        company_name = company_name[:company.find(' ')]
     else:
         company_name = 'Could not be found. Please submit'
     product = data[data.find('attributes')+24:data.find('description')-3]
-    print product
     product_name = ''
     for i in range(0, len(product)):
         if product[i] != '\\':
@@ -39,14 +41,11 @@ def get_data(upc_code):
     return (product_name,company_name)
         
 
-# use this for testing
 def get_test():
     '''() -> str
     test without getting from barcode
     '''
-    return ("Kellog's Rice Krispies Squares/Original","Kellogg")
+    return ("Kellog's Rice Krispies Squares/Original"),("Kellogg")
 
-# use this for processing everythings
 def process_image(fileName):
-    return get_data(get_upc(filename))
-
+    return get_data(get_upc(fileName))
